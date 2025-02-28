4825b4ea4ad7ed40468225889ba713075d595512
Guilherme Gabriel Ziger Granville <161463539+guilherme-granville@users.noreply.github.com>
2025-02-28 11:46:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,167 @@
+document.getElementById("cards").innerHTML = `
+    <div class="card">
+        <img src="cards/blue/0_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/1_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/2_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/3_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/4_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/5_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/6_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/7_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/8_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/9_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/2plus_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/block_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/blue/inverse_blue.png">
+    </div>
+    <div class="card">
+        <img src="cards/yellow/0_yellow.png">
+    </div>
+    <div class="card">
+        <img src="cards/yellow/1_yellow.png">
+    </div>
+    <div class="card">
+        <img src="cards/yellow/2_yellow.png">
+    </div>
+    <div class="card">
+        <img src="cards/yellow/3_yellow.png">
+    </div>
+    <div class="card">
+        <img src="cards/yellow/4_yellow.png">
+    </div>
+    <div class="card">
+        <img src="cards/yellow/5_yellow.png">
+    </div>
+    <div class="card">
+        <img src="cards/yellow/6_yellow.png">
+    </div>
+    <div class="card">
+        <img src="cards/yellow/7_yellow.png">
+    </div>
+    <div class="card">
+        <img src="cards/yellow/8_yellow.png">
+    </div>    
+    <div class="card">
+        <img src="cards/yellow/9_yellow.png">
+    </div>
+    <div class="card">    
+        <img src="cards/yellow/2plus_yellow.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/yellow/block_yellow.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/yellow/inverse_yellow.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/0_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/1_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/2_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/3_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/4_red.png">    
+    </div>    
+    <div class="card">    
+        <img src="cards/red/5_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/6_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/7_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/8_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/9_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/2plus_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/block_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/red/inverse_red.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/0_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/1_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/2_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/3_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/4_green.png">        
+    </div>    
+    <div class="card">    
+        <img src="cards/green/5_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/6_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/7_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/8_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/9_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/2plus_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/block_green.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/green/inverse_green.png">    
+    </div>    
+    <div class="card">    
+        <img src="cards/card back/card_back.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/wild/wild_card.png">
+    </div>    
+    <div class="card">    
+        <img src="cards/wild/4_plus.png">
+    </div>
+`
